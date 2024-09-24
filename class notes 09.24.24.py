755ab49b9616bb8843cb3245ef3f335380dfb18a
mkduran <mkduran@miners.utep.edu>
2024-09-24 09:25:50 -0600

    Create class notes 09.24.24.py

---
+++ class notes 09.24.24.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+filename= "./big-mac-full-index.csv"
+df = pd.read_csv(filename)
+
+# query = f"(iso_a3 == 'MEX')"
+# #query = f"(iso_a3 =='MEX' and (date > 2019-01-01 and date < 2019-12-31))"
+# mxn_df = df.query(query)
+
+# #print(mxn_df)
+# print(mxn_df['dollar_price'].min())
+# print(mxn_df['dollar_price'].max())
+# print(round(mxn_df)['dollar_price'].mean(),2)
+
+# query =f"(iso_a3 == 'JPN')"
+# jpn_df = df.query(query)
+# min_idx = jpn_df['dollar_price'].idxmax()
+
+# print(min_idx)
