9926ac24fdc4d9c8fe4de986b1d511f351430193
mkduran <mkduran@miners.utep.edu>
2024-09-26 09:04:36 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,16 +1,16 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year,country_code):
-    df = pd.read_csv('./big-mac-full-index.csv')
     df['date'] = pd.to_datetime(df['date'])
     df['year']=df['date'].dt.year
     df_year_iso = df[['year','iso_a3']]
     print(df_year_iso)
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    df_country_code = df['iso_a3']
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
