034b0ce8912cdc789c4e98e785f777a99bbee30d
mkduran <mkduran@miners.utep.edu>
2024-09-26 10:16:25 -0600

    restarting
    Here im restarting the assignment

--- class notes 09.24.24 part 2.py
+++ class notes 09.24.24 part 2.py
@@ -13,9 +13,9 @@ df = pd.read_csv('./big-mac-full-index.csv')
 # print(df)
 
 # country_code = "JPN" # i dont need to do this on Code 4
-df['iso_a3'] = df['iso_a3'].str.lower()
-query_text = df['iso_a3']
-print(query_text)
+# df['iso_a3'] = df['iso_a3'].str.lower()
+# query_text = df['iso_a3']
+# print(query_text)
 
 # # print(len(df))
 # sub_df = df.query(query_text)
@@ -30,8 +30,8 @@ print(query_text)
 # print(sub_df['dollar_price'].mean())
 
 
-# new_query = f"date > '2012-01-01' and date < '2012-12-31'"
+new_query = f"date > '109-01-01' and date < '2024-12-31'"
 
-# new_date_df = df.query(new_query)
+new_date_df = df.query(new_query)
 
-# print(new_date_df)
+print(new_date_df)
--- code_4.py
+++ code_4.py
@@ -3,12 +3,9 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
 
+
 def get_big_mac_price_by_year(year,country_code):
-    df['date'] = pd.to_datetime(df['date'])
-    df['year']=df['date'].dt.year
-    df['iso_a3']=df['iso_a3'].str.lower()
-    df_year_iso = df[['year','iso_a3']]
-    print(df_year_iso)
+    pass
 
 def get_big_mac_price_by_country(country_code):
      pass # Remove this line and code your function
@@ -20,5 +17,4 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    result_a = get_big_mac_price_by_year(2010,'arg')
-    print(result_a)
+    pass
