49159f1aa16a55aef9a4f270361e10f120f8ebca
mkduran <mkduran@miners.utep.edu>
2024-09-26 19:43:03 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -13,16 +13,16 @@ def get_big_mac_price_by_year(year,country_code):
     #df = pd.read_csv('./big-mac-full-index.csv')
     df['year']= df['date'].str[:4]
     df['iso_a3']=df['iso_a3'].str.lower()
-    df['dollar_price'] = df['dollar_price'].mean()
-    year_country_price=df[['year','iso_a3','dollar_price']]
-    print(round(year_country_price),2)
+    price = df['dollar_price'].mean().round(2)
+    year_country_price=df[['year','iso_a3']].copy()
+    year_country_price['price'] = 
+    return
 
 def get_big_mac_price_by_country(country_code):
-    pass
-    # df['iso_a3']=df['iso_a3'].str.lower()
-    # df['dollar_price'] = df['dollar_price'].mean()
-    # country_price=df[['iso_a3','dollar_price']]
-    # return(country_price)
+    df['iso_a3']=df['iso_a3'].str.lower()
+    df['dollar_price'] = df['dollar_price'].mean()
+    country_price=df[['iso_a3','dollar_price']]
+    print(country_price)
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
@@ -30,9 +30,5 @@ def get_the_cheapest_big_mac_price_by_year(year):
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
-get_big_mac_price_by_year()
-
 if __name__ == "__main__":
     pass
-    # result_a = get_big_mac_price_by_year(2010,'arg')
-    # print(result_a)
