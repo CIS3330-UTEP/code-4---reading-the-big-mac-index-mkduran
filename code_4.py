e31728f7d83d1f7fbdc5a08df53dcf60c3b0e7d2
mkduran <mkduran@miners.utep.edu>
2024-09-29 22:21:02 -0600

    Update code_4.py
    trying to figure out the date still to clean it up

--- code_4.py
+++ code_4.py
@@ -3,6 +3,7 @@ import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
 cc = set(df['iso_a3'].str.lower())
+
 def get_big_mac_price_by_year(year,country_code):
     #cc = set(df['iso_a3'].str.lower()) # removed from code to have for the rest of code 4
     if country_code in cc:
@@ -18,7 +19,13 @@ def get_big_mac_price_by_country(country_code):
 
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    years = df[df('date').st      r]
+    cheap_mac = years['dollar_price'].min()
+    bob = years.query('dollar_price == @cheap_mac')
+    name = bob['name'].iloc[0]
+    mac = bob['iso_a3'].iloc[0].upper()
+
+    return f"{name}({mac}): ${cheap_mac:.2f}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
@@ -28,3 +35,5 @@ if __name__ == "__main__":
     print(result_a)
     result_b = get_big_mac_price_by_country("mex")
     print(result_b)
+    result_c = get_the_cheapest_big_mac_price_by_year(2008)
+    print(result_c)
