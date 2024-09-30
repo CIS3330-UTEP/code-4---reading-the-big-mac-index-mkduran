d73c71d084dbaa0ab0e03773e3795c99fd93d086
mkduran <mkduran@miners.utep.edu>
2024-09-29 21:57:12 -0600

    Update code_4.py
    code not working i  believe it is with all the country codes therfore i will be hardcoding the country codes

--- code_4.py
+++ code_4.py
@@ -2,16 +2,14 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
-cc = df['iso_a3'].str.lower()
-#print(cc)
-years = df['date'].str[:4]
-#print(years)
 
 def get_big_mac_price_by_year(year,country_code):
-    if country_code in cc:
-        query = df(df['iso_a3'] == cc) & (df['date'] == years)
-        round1 = round(query['dollar_price'].mean(),2)
-        return round1
+    cc = df['iso_a3'].str.lower()
+    if country_code.lower() in cc:
+        query = df[(df['iso_a3'].str.lower() == country_code.lower()) & (df['date'].str.startswith(str(year)))]
+        rounda = round(query['dollar_price'].mean(),2)
+    
+    return rounda
 
 def get_big_mac_price_by_country(country_code):
     pass
@@ -24,3 +22,5 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 
 if __name__ == "__main__":
     pass
+    # result_a = get_big_mac_price_by_year(2010,"arg")
+    # print(result_a)
