b93a1fd01e404d42f02b69449616708c05c65d54
mkduran <mkduran@miners.utep.edu>
2024-09-29 21:20:12 -0600

    Update code_4.py
    Im restarting everything

--- code_4.py
+++ code_4.py
@@ -4,17 +4,7 @@ big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year,country_code):
-    df['year'] = df['date'].str[:4]
-    df['iso_a3'] = df['iso_a3'].str.lower()
-    year_country = df[(df['year'] == year) & (df['iso_a3'] == country_code)]
-    mean_price = year_country['dollar_price'].mean()
-    return(round(mean_price,2))
-    # df['year']= df['date'].str[:4]
-    # df['iso_a3']=df['iso_a3'].str.lower()
-    # price = df['dollar_price'].mean().round(2)
-    # year_country_price=df[['year','iso_a3']].copy()
-    # year_country_price['price'] = price
-    # print(year_country_price)
+    pass
 
 def get_big_mac_price_by_country(country_code):
     pass
